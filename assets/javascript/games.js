d2d08e882132bb5c2e54b7fc7ffe069609c3d9c5
Haralambos <lampistsivikas@yahoo.com>
2019-04-12 12:36:26 -0700

    new message

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,34 +1,15 @@
+var letters = ["a", "b", "c"]
+//This array will hold what we have guessed//
+var guessedLetters = [];
+// This variable will randomly assigned one of the 3 letters
+var letterToGuess = null;
+
+// We will use this in order to start counting down
+var wins = 0;
+var loses = 0;
 
 
-           var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-        var wins = 0;
-        var losses = 0;
-        var guesses = 0;
-        var guessesLeft = 9
-        var guessedLetters = [];
-        var lettertoGuess = null;
-
-        //Users Guess//
-
-        document.onkeyup = function (event) {
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase()
-          console.log('my user guess: ',userGuess)
-            //Computer selects a random number//
-
-            var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log('computer guess: ',computerGuess)
-            
-
-            if (userGuess == computerGuess) {
-                wins++;
-                console.log('i win')
-            } else  {
-                losses++;
-                console.log('i lose')
-            }
-
-        }
 
     
 
