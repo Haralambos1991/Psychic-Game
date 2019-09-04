f9325c62876e6e341a9971e335e8e43cdedd4edd
Haralambos <lampistsivikas@yahoo.com>
2019-09-04 14:24:16 -0700

    new message

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,70 +1,105 @@
-var letters = ["a", "b", "c"]
-//This array will hold what we have guessed//
-var guessedLetters = [];
-// This variable will randomly assigned one of the 3 letters
-var letterToGuess = null;
-
-// We will use this in order to start counting down
 var wins = 0;
-var loses = 0;
+var losses = 0;
+var guessesLeft = 9;
+var yourGuess = []; 
+var computerChoice;
 
-// Below we have created 3 functions in order to update the guess //
 
-var updateGuessesLeft = function() {
-// Here we are grabing the html element and setting it equal to the guessesLeft
 
-document.querySelector("#guesses-left").innerHTML = guessesleft;
-};
+//to initiate the game
+theGame();
 
-var updateLetterToGuess = function() {
-// Here we get a random letterToGuess and assign it based on a random generator
 
-lettersToGuess = letters[Math.floor(Math.random() * letters.length)]
-};
+function theGame() {
+	//letter randomizer
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var randomLetter = alphabet[Math.floor(Math.random() * 26)];
+	var computerChoice = randomLetter;
 
-var updateGuessesSoFar = function() {
- // Here we take the guess the user has tried //
- document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(",");   
-};
-// Function will be called when we reset everything //
+	console.log(computerChoice); //test
+		
+	checkIfCorrect();
 
-var reset = function() {
-guessesLeft = 9;
-guessedLetters = [];
-updateGuessesLeft();
-updateGuessesSoFar();
-};
+	function checkIfCorrect() {
 
-//Execute on page load. //
-updateLetterToGuess();
-updateGuessesLeft();
+		document.onkeyup = function(event) {
 
-//This function will capture the keyboards clicks//
-document.onkeydown = function(event) {
-    // Right now its gonna reduce guessesLeft by one //
-    guessesLeft--;
+			//turns user's unicode into corresponding alphanumeric key,
+			//then turns it into a lowercase string.
+			var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 
-    // Lowercase the letter
-    var letter = String.fromCharCode(event.which).toLowerCase();
 
-    // Then add the guess to the guessedLetters array
-    guessedLetters.push(letter);
+			//if statement to prevent user from choosing a key that's not part of
+			//the alphabet. a = unicode 65, z = unicode 90.
+			if (event.keyCode < 65 || event.keyCode > 90) {
+				alert("Invalid Entry");
 
-    //Then its going to run the update functions
-    updateGuessesLeft();
-    updateGuessesSoFar();
+			//else/if statement to stop user from guess a letter they already guessed
+			//and to stop the guesses counter from being deducted for a previous guess
+			} else if (yourGuess.indexOf(userChoice) >=0) {
+				alert("You already guessed that!");
 
-    // We will check if there is a match //
-    if (letter === letterToGuess) {
-     
-    // If there is, then we win and we will update the HTML //
-    wins++;
-    document.querySelector("#wins").innerHTML = wins;
+			//if the user guesses correctly
+			} else if (userChoice === computerChoice) {
+				console.log("You win."); //test
+				
+				//switches still image of black box to animated gif of light bulb flashing
+				document.getElementById("image-here").setAttribute("src", "assets/images/light-bulb.gif");
+				
+				//setTimeout() delays a block of code for x seconds (written in milliseconds). 
+				//in this case it will delay the You win! alert for 3 seconds, allowing the gif animation to cycle through. 
+				setTimeout(function(){alert("You win!");}, 3000);
+				wins = wins + 1;
+				document.getElementById("your-wins").innerHTML = wins;
 
-    // Then i will reset the game //
-    reset();
-    }
+				//this delays the reset of the black box image
+				setTimeout(function(){document.getElementById("image-here").setAttribute("src", "assets/images/black-box.png");}, 3000);
 
-  // If we are out of guesses...
-  if (guessesLeft === 0)  {
+				resetGame();
+
+
+			//if the user guesses wrong...
+			} else {
+				guessesLeft = guessesLeft - 1;
+				
+				document.getElementById("guesses-left").innerHTML = guessesLeft; 
+				yourGuess.push(userChoice); //append user's choice to array yourGuess
+
+				console.log("Your guesses so far: " + yourGuess); //test
+
+				document.getElementById("your-guesses").innerHTML = yourGuess;
+
+				console.log("Guesses Left: " + guessesLeft); //test
+
+				noGuessesLeft();
+			}
+		}
+	}
+
+	function resetGame() {
+		guessesLeft = 9; //reset variable
+		yourGuess = [];  //reset array so it's empty
+		document.getElementById("guesses-left").innerHTML = guessesLeft;  //reset display on screen
+		document.getElementById("your-guesses").innerHTML = yourGuess;    //reset display on screen
+		theGame(); //restart the game with new computerChoice.
+
+	}
+
+	function noGuessesLeft() {
+		if (guessesLeft === 0) {
+			console.log("YOU LOSE."); //test
+			alert("YOU LOSE!");
+			losses = losses + 1
+			document.getElementById("your-losses").innerHTML = losses;
+
+			resetGame();
+
+		} else {
+			console.log("Incorrect. Try again"); //test
+			checkIfCorrect();
+		}
+
+	}
+
+}
 
