daf91c79f6b1b28b7e1eb7cb7f6c28528111b622
Haralambos <lampistsivikas@yahoo.com>
2019-10-02 13:02:42 -0700

    new message

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,59 +1,61 @@
-// Here i am defining the users choices by specifing the element id//
-var wins= document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessesLeft = document.getElementById("guessesLeft");
-var guessesSoFar = document.getElementById("guessesSoFar");
-
 // Here is the array of the letters that the user will choose and below are the efforts remaing for him.
-var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var winsCount = 0;
-var lossesCount = 0;
-var defaultGuessesremaining = 9;
-var guessesArray = [];
+var computerChoices = [
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z"
+];
 
-// This code has been created for the user to choose a random letter
-/*var getRandomLetter = function() {
-randomLetter= allLetters[Math.floor(Math.random().allLetters.length)]
-console.log(randomLetter);
-return randomLetter;
-*/
-var displayStats = function() {
- wins.innerHTML = "Wins: " + winsCount
- losses.innerHTML = "Losses: " + lossesCount
- guessesRemaining.innerHTML = "GuessesLeft: " + guessesRemainingCount
- guessesSoFar.innerHTML = "GuessesSoFar: " + guessesArray
-}
-//The game will be reset and start over again.
-//var resetGame = function() {
-  /*guessesArray = [];
-  guessesRemainingCount = defaultGuessesRemaining
-  currentLetter = getRandomletter();
+var wins = 0;
+var loses = 0;
+var guessesLeft = 10;
+var lettersGuessed = [];
 
-  var userWon = function (userLetter) {
-    return userLetter === currentLetter
+window.onload = function() {
+	var computerGuess = String.fromCharCode[Math.floor(Math.random).computerChoices.length];
+	computerGuess.push(computerGuess);
 
-  var userLost = function() {
-    return guessesRemainingCount === 0;
-  }
-*/
-var currentLetter = getRandomLetter(); {
-  displayStats()
-  document.onkeyup = function(_event) {
-   guessesRemainingCount --;
-   guessesRemaining.push(userLetters)
+	console.log(computerGuess);
 
-   if (userWon(userLetter))
-      winsCount++
-      resetGame();
-  }
+	// Function to capture user's keyboard input //
 
-  if (userLost)
-     lostCount++
-     resetGame();
-}
+	document.onkeydown = function(event) {
+		var keyPress = String.fromCharCode(event.KeyCode).toLowerCase();
 
-displayStats();
-  
+		addLetter = keypress;
+	};
 
+	// Function to catch repeatLetters
 
-//
+	function addLetter(usersKeypress) {
+		var repeatGuess = lettersGuessed.some(function(item) {
+			return item === usersKeypress;
+		});
+
+		if (repeatGuess) {
+			alert(userKeypress + " already guessed. Try again!");
+		}
+	}
+};
